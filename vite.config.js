95e90029152d9f72ad5a4626dfe1fe0a4e10776a
sohelllkhan <sohelkhan323526@gmail.com>
2025-07-15 01:17:57 +0600

    edited

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/sohelllkhan.github.io//",
   plugins: [react(),tailwindcss(),]
 })
